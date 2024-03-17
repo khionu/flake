c0e4ef5be7a7cc182361bb46cb2000d0300b2f6c
Khionu Sybiern <dev@khionu.net>
2024-03-16 19:18:03 -0700

    feat: import homeconfigs via systemd unit

--- traits/hm.nix
+++ traits/hm.nix
@@ -1,7 +1,38 @@
-{ home-manager, config, globals, pkgs,... }@inputs: {
+{ home-manager, config, globals, pkgs,... }@inputs:
+let
+  manageHomeConfigPkg = pkgs.nuenv.writeScriptBin {
+    name = "upsert-hm";
+    script = ''
+      let hm_path = path join $env.home ".config/home-manager"
+      if not ( $hm_path | path exists) {
+        
+      }
+    '';
+  };
+in {
   imports = [
     home-manager.nixosModules.home-manager
   ];
+
+  options.sybiern.users = 
+    (import <nixpkgs/nixos/modules/services/networking/ssh/sshd.nix> inputs)
+    .options.users.users;
+
+  users.users = config.sybiern.users;
+  
+  systemd.user.units = {
+    "pullHome" = {
+      script = ''
+      mkdir -p
+      '';
+      serviceConfig = {
+        RootDirectory = "/etc/nixos";
+        RuntimeDirectory = "nixos";
+      };
+    };
+  };
+
+
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
