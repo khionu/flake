6587921eab4389aca07d5bb98de9a05e78bf5e66
Khionu Sybiern <dev@khionu.net>
2026-02-26 10:26:09 -0800

    chore: fix Tailscale to use new option

--- traits/tailscale.nix
+++ traits/tailscale.nix
@@ -1,5 +1,7 @@
 { ... }: {
   meta.properties = [ "trait.tailscale" ];
+  
   networking.firewall.checkReversePath = "loose";
   services.tailscale.enable = true;
+  services.tailscale.openFirewall = true;
 }
